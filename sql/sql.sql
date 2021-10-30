ac3378aaa3e2240ac6cc6cc30ccb23f22ee1832b
marcelo_alves <mamdria@gmail.com>
2021-10-30 18:44:02 -0300

    criando layout painel3

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,27 +1,23 @@
 drop table if exists estoque ;
-CREATE TABLE estoque(  
-    id_estoque int(11) NOT NULL primary key AUTO_INCREMENT,
-    id_produto int(11),
-    quantidade int(11),
-	valor_compra decimal(10,2) default 0.00,
-	valor_venda decimal(10,2) default 0.00,
-    lote VARCHAR(120),
-	validade TIMESTAMP,
-	fabricacao TIMESTAMP,
-    data_atualizar TIMESTAMP
-) ;
+CREATE TABLE estoque (
+  id_estoque int NOT NULL AUTO_INCREMENT,
+  id_produto int DEFAULT NULL,
+  quantidade int DEFAULT NULL,
+  lote varchar(120) DEFAULT NULL,
+  validade timestamp NULL DEFAULT NULL,
+  data_atualizar timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (id_estoque)
+)
 
 drop table if exists produto;
-CREATE TABLE produto(  
-    id_produto int(11) NOT NULL primary key AUTO_INCREMENT,
-	id_fornecedor int(11),
-	id_categoria int(11),
-    nome varchar(100),
-    validade int(1),
-    validade_dias int(5),    
-    qtde int(11),    
-    data_criar TIMESTAMP,
-    data_atualizar TIMESTAMP
+CREATE TABLE produto (
+  id_produto int NOT NULL AUTO_INCREMENT,
+  id_fornecedor int DEFAULT NULL,
+  id_categoria int DEFAULT NULL,
+  nome varchar(100) DEFAULT NULL,
+  data_criar timestamp NULL DEFAULT NULL,
+  data_atualizar timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (id_produto)
 ) ; 
 
 drop table if exists categoria;
@@ -86,12 +82,22 @@ CREATE TABLE fornecedor(
 	data_criar TIMESTAMP,
     data_atualizar TIMESTAMP
 );
+drop table if exists valor_venda;
+CREATE TABLE valor_venda (
+  id_valor int NOT NULL AUTO_INCREMENT,
+  id_produto int NOT NULL,
+  valor_venda decimal(10,2) DEFAULT '0.00',
+  valor_compra decimal(10,2) DEFAULT '0.00',
+  valor_atual smallint DEFAULT '1',
+  data_atualizar timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (id_valor)
+) ;
 
 drop view if exists visao_estoque;
 CREATE VIEW  visao_estoque
 AS
 SELECT 
-F.nome as fornecedor,E.id_estoque ,P.id_produto,P.nome as nome, P.id_categoria, C.nome as categoria, E.lote, E.valor_compra,E.valor_venda,P.validade, P.validade_dias,E.quantidade 
+F.nome as fornecedor,E.id_estoque ,P.id_produto,P.nome as nome, P.id_categoria, C.nome as categoria, E.lote,E.quantidade 
 FROM 
 estoque E INNER JOIN  produto P ON E.id_produto=P.id_produto INNER JOIN fornecedor F on P.id_fornecedor=F.id_fornecedor
 INNER JOIN categoria C on P.id_categoria=C.id_categoria;
@@ -99,7 +105,7 @@ INNER JOIN categoria C on P.id_categoria=C.id_categoria;
 drop trigger if exists TR_ESTOQUE;
 CREATE DEFINER =`root`@`localhost` 
 TRIGGER TR_ESTOQUE AFTER INSERT ON produto FOR EACH ROW
-insert into estoque(id_produto,quantidade,valor_compra,	valor_venda) value (NEW.id_produto, 0, 0.00, 0.00);
+insert into estoque(id_produto,quantidade) value (NEW.id_produto, 0, 0.00, 0.00);
 
 insert into fornecedor values (null,'PADRÃO','00.000.000/0000-00',NOW(),null);
 

ba18d5cc5c203296ab6748b17009f59238b9f0bd
MARCELO ALVES <mamdria@gmail.com>
2022-04-14 19:28:10 -0300

    Add files via upload

--- controller/login.php
+++ controller/login.php
@@ -5,18 +5,49 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1); //*/
 
 include_once './model/busca.php';
-include_once './model/inserir.php';
-include_once './model/alterar.php';
-include_once './model/deletar.php';
 
 class Login{
 	public static function lista(){
-		$usuario =$_POST['usuario'];
-		$senha =$_POST['senha'];
-		$funcionario = busca::buscaWhere('*','funcionario','and usuario="'.$usuario.'" and senha="'.$senha.'"');
-		//$funcionario = busca::buscaTudo('f.id_funcionario,f.nome,f.cpf,f.email,n.nome as nivel','funcionario f inner join nivel n on f.id_nivel=n.id_nivel');
-		print_r($funcionario);
-		//return $funcionario;
+		
+		$POST = json_encode($_POST, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+		$POST = json_decode($POST);
+		
+		$usuario =  html_entity_decode($POST->usuario);
+		$senha = html_entity_decode($POST->senha);	
+		
+		
+		$tabela = 'funcionario f inner join nivel n on f.id_nivel = n.id_nivel';
+		$campos = 'f.nome as nome, f.email, f.usuario, n.caixa, n.venda, n.estoque, n.produto, n.usuario, 
+					n.fornecedor, n.empresa, n.sangria, n.excluir_item, n.relatorio, n.desconto ,n.valor_desconto';
+		$condicao = ' and f.usuario="'.$usuario.'" and f.senha="'.$senha.'"';
+		$funcionario = busca::buscaWhere($campos,$tabela,$condicao);
+		
+		
+		if($funcionario){
+			session_start();
+			
+			foreach($funcionario[0] as $cha => $fun){
+				$_SESSION[$cha] = $fun;				
+			}
+			
+			foreach($funcionario  as $cha => $fun){
+					if( $fun->estoque ==1){
+						echo '{"nivel":"estoque"}';
+					}elseif( $fun->venda ==1){
+						echo '{"nivel":"venda"}';
+					}elseif( $fun->caixa ==1){
+						echo '{"nivel":"caixa"}';
+					}	
+			}
+			
+		}else{
+			echo '{"nivel":"erro"}';
+		}
+		
+		
+		
+		
+		//return $funcionario;*/
 	}
 	
 }

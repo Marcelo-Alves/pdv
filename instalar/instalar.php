0e052526dd82da974143d4160894cdf61bdc0905
Marcelo-Alves <mamdria@gmail.com>
2022-11-09 20:28:48 +0000

    instalar 1728

--- instalar/instalar.php
+++ instalar/instalar.php
@@ -1,6 +1,8 @@
 <?php
 include_once '../model/mysql.php';
 try{
+    $sql = // Carrega o arquivo .SQL
+    $file = file_get_contents("bdVirgem.sql");
     $rs = mysql::conexao()->prepare($sql);  
     $rs->execute();
 }
